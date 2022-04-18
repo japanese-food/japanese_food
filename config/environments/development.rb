396c48e78a74fdf157696e6a4b3e3db25e069599
Tamami Higashino <100408560+tamam03@users.noreply.github.com>
2022-04-18 15:25:02 +0900

    Revert "Userinfomation"

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,6 +73,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-config.hosts << "052653bb9fc94907b52d0720945d6195.vfs.cloud9.ap-northeast-1.amazonaws.com"
-config.hosts << "564314732fdb4a508cc2bca510a355fb.vfs.cloud9.us-east-1.amazonaws.com"
 end
